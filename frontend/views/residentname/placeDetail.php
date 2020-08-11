99f013d6c167ba12465e6544a88be8af6a4cf043
mgnedash <gnedasch2011@yandex.ru>
2020-08-11 15:45:09 +0300

    fix

--- frontend/views/residentname/placeDetail.php
+++ frontend/views/residentname/placeDetail.php
@@ -43,32 +43,32 @@
 
 
     </div>
-
-    <table class="table table-striped">
-        <tr>
-            <th>Падеж</th>
-            <th>Вопрос</th>
-            <th>Страна</th>
-            <th>Вопрос</th>
-            <th>Мужчина</th>
-            <th>Женщина</th>
-            <th>Жители</th>
-        </tr>
-        <tbody>
-
-        <?php foreach ($nounsesGroup as $caseName => $casesGroup): ?>
+    <div class="tableScroll">
+        <table class="table table-striped">
             <tr>
-                <th scope="row"><?= $caseName; ?></th>
-                <td><?= $casesGroup['question_what']; ?></td>
-                <td><?= $casesGroup[0]->value; ?></td>
-                <td><?= $casesGroup['question_who']; ?></td>
-                <td><?= $casesGroup[1]->value; ?></td>
-                <td><?= $casesGroup[2]->value; ?></td>
-                <td><?= $casesGroup[3]->value; ?></td>
+                <th>Падеж</th>
+                <th>Вопрос</th>
+                <th>Страна</th>
+                <th>Вопрос</th>
+                <th>Мужчина</th>
+                <th>Женщина</th>
+                <th>Жители</th>
             </tr>
-        <?php endforeach; ?>
-    </table>
+            <tbody>
 
+            <?php foreach ($nounsesGroup as $caseName => $casesGroup): ?>
+                <tr>
+                    <th scope="row"><?= $caseName; ?></th>
+                    <td><?= $casesGroup['question_what']; ?></td>
+                    <td><?= $casesGroup[0]->value; ?></td>
+                    <td><?= $casesGroup['question_who']; ?></td>
+                    <td><?= $casesGroup[1]->value; ?></td>
+                    <td><?= $casesGroup[2]->value; ?></td>
+                    <td><?= $casesGroup[3]->value; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
     <br>
     <br>
     <br>
@@ -76,29 +76,30 @@
 
     <?php if (isset($place->cities) && !empty($place->cities)): ?>
         <h2>Города <?= $place->genitive->value; ?></h2>
-    <div class="tableScroll">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Город</th>
-                <th>Мужчины</th>
-                <th>Женщины</th>
-                <th>Граждане</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            <?php foreach ($place->cities as $city): ?>
+        <div class="tableScroll">
+            <table class="table table-striped">
+                <thead>
                 <tr>
-                    <th scope="row"><a href="<?= $city->url->url; ?>"><?= $city->name; ?></a></th>
-                    <td><?= $city->man; ?></td>
-                    <td><?= $city->woman; ?></td>
-                    <td><?= $city->townspeople; ?></td>
+                    <th>Город</th>
+                    <th>Мужчины</th>
+                    <th>Женщины</th>
+                    <th>Граждане</th>
                 </tr>
-            <?php endforeach; ?>
+                </thead>
+                <tbody>
 
-        </table>
-    </div>
+                <?php foreach ($place->cities as $city): ?>
+                    <tr>
+                        <th scope="row"><a href="<?= $city->url->url; ?>"><?= $city->name; ?></a>
+                        </th>
+                        <td><?= $city->man; ?></td>
+                        <td><?= $city->woman; ?></td>
+                        <td><?= $city->townspeople; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+            </table>
+        </div>
     <?php endif; ?>
 
 
@@ -138,7 +139,7 @@
         font-size: 1.3em;
     }
 
-    .tableScroll{
+    .tableScroll {
         overflow-x: scroll;
     }
 </style>
