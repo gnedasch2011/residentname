4fcd7d8391c1467db4212719a3b145e818c1d1f0
mgnedash <gnedasch2011@yandex.ru>
2020-08-20 17:38:55 +0300

    h1 1

--- frontend/views/residentname/index.php
+++ frontend/views/residentname/index.php
@@ -93,9 +93,7 @@ $country = true;
 $country = false;
 ?>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <?php if (isset($h1)): ?>
-        <h1><?= $h1; ?></h1>
-    <?php endif; ?>
+
     <style>
         table {
             width: 100%;
